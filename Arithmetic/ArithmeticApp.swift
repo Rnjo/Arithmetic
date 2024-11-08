364037f54997f9e535fddbcf0b8a4d2506d363a6
Rnjo <kotamraju.mihir@gmail.com>
2024-11-08 09:26:42 -0800

    Initial Commit

---
+++ Arithmetic/ArithmeticApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticApp.swift
+//  Arithmetic
+//
+//  Created by Mihir Kotamraju on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArithmeticTests/ArithmeticTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticTests.swift
+//  ArithmeticTests
+//
+//  Created by Mihir Kotamraju on 11/8/24.
+//
+
+import Testing
+@testable import Arithmetic
+
+struct ArithmeticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
